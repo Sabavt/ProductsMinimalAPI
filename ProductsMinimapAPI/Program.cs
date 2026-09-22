c61a846114ed9a61580850294de68bc6254ac662
Sabavt <envecta.info@proton.me>
2026-09-22 18:32:48 +0400

    Refactor products API into extension method for modularity
    Moved products endpoints from Program.cs to ProductsMapGroup.ProductsAPI extension method in ProductsMinimapAPI.RouteGroups. Registered endpoints via app.MapGroup("/products").ProductsAPI() for improved organization and maintainability.

--- ProductsMinimapAPI/Program.cs
+++ ProductsMinimapAPI/Program.cs
@@ -1,27 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
-using ProductsMinimapAPI.Models;
+using ProductsMinimapAPI.RouteGroups; 
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Product> products = [
-    new Product(){ProductName = "Orange", Id = 1},
-    new Product(){ProductName = "Apple", Id = 2},
-    new Product(){ProductName = "Banana", Id = 3} 
-    ];
-
-app.MapGet("/products", async (HttpContext context) =>
-{
-    var products_text = string.Join("\n", products.Select(p => p.ToString()));
-
-    await context.Response.WriteAsync(products_text);
-});
-
-app.MapPost("/", async (HttpContext context, Product product) =>
-{
-    products.Add(product); 
-
-    await context.Response.WriteAsync($"Added product with ID: {product.Id}");
-});
-
+var routeGroup = app.MapGroup("/products").ProductsAPI();
+ 
 app.Run();
---
+++ ProductsMinimapAPI/RouteGroups/ProductsMapGroup.cs
@@ -0,0 +1,37 @@
+﻿using ProductsMinimapAPI.Models; 
+using System.Text.Json;
+
+namespace ProductsMinimapAPI.RouteGroups;
+
+public static class ProductsMapGroup
+{
+    private static List<Product> products = [
+            new Product(){ProductName = "Orange", Id = 1},
+    new Product(){ProductName = "Apple", Id = 2},
+    new Product(){ProductName = "Banana", Id = 3}
+            ];
+
+    public static RouteGroupBuilder ProductsAPI(this RouteGroupBuilder routeGroup)
+    { 
+        routeGroup.MapGet("/", async (HttpContext context) =>
+        {
+            await context.Response.WriteAsync(JsonSerializer.Serialize(products));
+        });
+
+        routeGroup.MapGet("/{Id:int}", async (HttpContext context, int Id) =>
+        {
+            var matchingProduct = products.SingleOrDefault(t => t.Id == Id);
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(matchingProduct));
+        });
+
+        routeGroup.MapPost("/", async (HttpContext context, Product product) =>
+        {
+            products.Add(product);
+
+            await context.Response.WriteAsync($"Added product with ID: {product.Id}");
+        });
+
+        return routeGroup;
+    } 
+}
