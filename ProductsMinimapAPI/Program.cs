1a916323bb3f3d99c9739a2e7a016d3fe72fbb5f
Sabavt <envecta.info@proton.me>
2026-09-22 21:35:38 +0400

    Add Serilog logging with Seq sink support
    Added Serilog and related packages to the project. Configured Serilog as the logging provider in Program.cs, reading settings from configuration. Updated appsettings.json to include Serilog configuration with a Seq sink for log output.

--- ProductsMinimapAPI/Program.cs
+++ ProductsMinimapAPI/Program.cs
@@ -1,6 +1,15 @@
-using ProductsMinimapAPI.RouteGroups; 
+using ProductsMinimapAPI.RouteGroups;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((HostBuilderContext builder, IServiceProvider services, LoggerConfiguration logger) =>
+{
+    logger
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(services);
+});
+
 var app = builder.Build();
 
 var routeGroup = app.MapGroup("/products").ProductsAPI();
